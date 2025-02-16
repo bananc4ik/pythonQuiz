b5972b708b208b2216cb80670a548d4956c41fd9
bananc4ik <rkononcuk03@gmail.com>
2025-02-16 16:57:01 +0300

    UPDATE

--- main.js
+++ main.js
@@ -9,17 +9,25 @@ started_button.addEventListener("click", function (event) {
 });
 let user_score = "";
 let question_options = document.getElementById("question_options");
-let question_buttons = question_options.getElementsByTagName("p");  
-console.log(element);
-function reaction(event) {
+let question_buttons = question_options.getElementsByTagName("p");
+function reaction (event) {
     for (let n = 0; n < question_buttons.length; n++) {
-        question_buttons[n].style.border = "1px solid #dddddd"
-        event.target.style.border = "1px solid #0659F3"
+        question_buttons[n].style.border = "1px solid #dddddd";
+        event.target.style.border = "1px solid #0659F3";
         
     }
-    
-    
-    
 }
+question_but.addEventListener("click", function (event) {
+    for (let j = 0; j < question_buttons.length; j++) {
+        if (question_buttons[j].style.border = "1px solid #0659F3") {
+            console.log("aaaaa");
+            
+    
+        }
+        
+    }
+
+    
+})
 
 
