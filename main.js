2badba81959d5d0dd09f89067b7a2e3642def045
bananc4ik <rkononcuk03@gmail.com>
2025-02-16 18:45:46 +0300

    MY_NEW_UPDATE

---
+++ main.js
@@ -0,0 +1,25 @@
+let started_box = document.getElementById("started_box");
+let started_button = document.getElementById("started_button");
+let question_box = document.getElementById("question_box");
+started_button.addEventListener("click", function (event) {
+    started_box.style.visibility = "collapse";
+    question_box.style.visibility = "visible";
+
+
+});
+let user_score = "";
+let question_options = document.getElementById("question_options");
+let question_buttons = question_options.getElementsByTagName("p");  
+console.log(element);
+function reaction(event) {
+    for (let n = 0; n < question_buttons.length; n++) {
+        question_buttons[n].style.border = "1px solid #dddddd"
+        event.target.style.border = "1px solid #0659F3"
+        
+    }
+    
+    
+    
+}
+
+
