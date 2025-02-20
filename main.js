82f36914cbd6e2b79a083c37e269d40972673ec9
bananc4ik <rkononcuk03@gmail.com>
2025-02-20 15:41:59 +0300

    MY_UPDATE

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let question_list = document.getElementById("question_options");
+let question_option = document.querySelector('input[name ="radio_button"]:checked').value;
+console.log(question_option);
+
+let question_label = document.querySelectorAll("label");
 let object = [
     {
         question:"Какая функция выводит что-либо в консоль?",
@@ -10,15 +13,14 @@ let object = [
 let question_number = 0;
 let question_title = document.getElementById("question_title");
 question_title.textContent = object[question_number].question;
-
-let question_list_child = question_list.children;
-console.log(object[0].options);
+let inputs = document.querySelectorAll("input");
 
 let index = 0;
 object[0].options.forEach(element => {
     
-    let a = question_list_child[index]
+    let a = question_label[index]
     a.textContent = element;
+    inputs[index].value = element;
     index += 1
     console.log(a);
     
@@ -28,11 +30,15 @@ object[0].options.forEach(element => {
 
 
 
-
+let question_button = document.getElementById("question_but");
 let started_button = document.getElementById("started_button");
 let question_box = document.getElementById("question_box");
 started_button.addEventListener("click", function (event) {
     started_box.style.visibility = "collapse";
     question_box.style.visibility = "visible";
-
 });
+question_button.addEventListener("click",function (event) {
+    console.log(question_option);
+    
+    
+})
